2c2e5120444e09031eb58df11e91bc9d8dfcc0fe
kaschmid <kara.schmidt@moccamedia.com>
2023-10-18 12:23:46 +0200

    cleanup + first arena mockup

---
+++ src/main/resources/static/JavaScript/BotScripts/BotLogic.js
@@ -0,0 +1,21 @@
+function botTurn() {
+
+    //start turn
+    untapCards(false);
+    drawCard(false);
+
+    //try to play cards
+    for (let index = 0; index < botHand.length; ++index) {
+        botHand[index].playCard();
+    }
+
+    //attack
+    for (let index = 0; index < BotHand.length; ++index) {
+        botField[index].tapCard("attack");
+    }
+
+    //end turn
+    while (botHand.length > 7) {
+        botHand.pop();
+    }
+}
